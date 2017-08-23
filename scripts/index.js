7ea119b713a4d320f22db64f3860cc24ac3a81bd
T.Jay Rocket <tjayrocket@gmail.com>
2017-08-23 11:21:50 -0700

    Working on hero rotating carousel

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let timeLapse = 5;
+let current = 0;
+
+let hero1 = '../assets/current-heros/logohero.png';
+let hero2 = '../assets/current-heros/logohero.png';
+let hero3 = '../assets/current-heros/logohero.png';
+
+let currentHeros = [hero1, hero2, hero3];
+
+console.log(currentHeros);
+
+function heroRotate(){
+  console.log('hello');
+}
+
+heroRotate();
+
